58cf31ad33abc7332d2da941595686dfce92f119
asheshyr <asheshyr@gmail.com>
2022-11-05 22:10:43 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a minesweeper clone project coded in python
+first version can generate a board with set dimensions and number of mines
+the grid is made of a list of lists
---
+++ main.py
@@ -0,0 +1,61 @@
+import random as r
+
+
+class Board(): #Create a grid object based on input dimensions
+	def __init__(self, width, height, bombs):
+		self.wd = width
+		self.ht = height
+		self.grid = [[0 for x in range(width)] for x in range(height)]
+		self.bombs = bombs
+
+	
+class Bomb(): #create bomb object
+	def __init__(self, board):
+		self.name = 'X'
+		#create random coordinates when bomb object is initialized
+		self.x = r.randint(0, board.wd-1)
+		self.y = r.randint(0, board.ht-1)
+
+
+def increment_tile(board, tile_y, tile_x):
+	try:
+		board.grid[tile_y][tile_x] += 1
+	except (IndexError, TypeError):
+		pass
+
+
+def increment_surrounding(board, y , x):
+	
+	increment_tile(board, y + 1,x + 1)
+	increment_tile(board, y - 1,x - 1)
+	increment_tile(board, y + 1, x - 1)
+	increment_tile(board, y - 1,x + 1)
+	increment_tile(board, y + 1, x)
+	increment_tile(board, y - 1, x)
+	increment_tile(board, y, x + 1)
+	increment_tile(board, y, x - 1)
+
+
+
+def draw_board(board):
+
+	if board.bombs > board.wd * board.ht:
+		print('too many bombs')
+		return
+	else:	
+		while board.bombs != 0:
+			#create new bombs based on bomb value in board object
+			new_bomb = Bomb(board)
+	
+			#add bombs to the grid
+			if board.grid[new_bomb.y][new_bomb.x] != new_bomb.name:
+				board.grid[new_bomb.y][new_bomb.x] = new_bomb.name	
+				increment_surrounding(board, new_bomb.y, new_bomb.x)
+				board.bombs -= 1								
+	for line in board.grid:
+		print(line)
+	
+
+new_board = Board(8,8,10)
+
+draw_board(new_board)
